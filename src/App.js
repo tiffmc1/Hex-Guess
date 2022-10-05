1568971fee5f1ab893353abbe989332da96f5ce8
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2022-10-05 00:26:21 -0400

    handleResult() and set results to state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
 	const [color, setColor] = useState(null);
 	const [colors, setColors] = useState([]);
+	const [result, setResult] = useState(null);
 
 	useEffect(() => {
 		const firstColor = getRandomColor();
@@ -11,7 +12,9 @@ function App() {
 		const thirdColor = getRandomColor();
 
 		setColor(firstColor);
-		setColors([firstColor, secondColor, thirdColor]);
+		setColors(
+			[firstColor, secondColor, thirdColor].sort(() => Math.random() - 0.5)
+		);
 	}, []);
 
 	function getRandomColor() {
@@ -22,16 +25,33 @@ function App() {
 		return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 	}
 
+	function handleResult(singleColor) {
+		if (singleColor === color) setResult(true);
+		else setResult(false);
+	}
+
 	return (
 		<>
 			<div className="app-container">
 				<div className="app-color-box" style={{ background: color }}></div>
 				<div className="app-btn-container">
-					{colors.map((color) => (
-						<button className="app-btn" key={color}>
-							{color}
+					{colors.map((singleColor) => (
+						<button
+							className="app-btn"
+							key={singleColor}
+							onClick={() => handleResult(singleColor)}
+						>
+							{singleColor}
 						</button>
 					))}
+				</div>
+				<div className="app-result-container">
+					<div className="app-result-wrong">
+						{result === false ? "Wrong!" : null}
+					</div>
+					<div className="app-result-correct">
+						{result === true ? "Correct!" : null}
+					</div>
 				</div>
 			</div>
 		</>
